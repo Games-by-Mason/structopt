73ccfd8cc75b572398a7b11e390578de487bd2e0
Mason Remaley <mason@anthropicstudios.com>
2025-07-23 15:19:36 -0700

    Reuses structopt module for tests

--- build.zig
+++ build.zig
@@ -3,18 +3,14 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    _ = b.addModule("structopt", .{
+    const structopt = b.addModule("structopt", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
 
     const unit_tests = b.addTest(.{
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/root.zig"),
-            .target = target,
-            .optimize = optimize,
-        }),
+        .root_module = structopt,
     });
     const run_unit_tests = b.addRunArtifact(unit_tests);
     const test_step = b.step("test", "Run unit tests");
